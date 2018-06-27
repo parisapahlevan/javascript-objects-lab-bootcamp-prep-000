ed4188f4c5745af518250a50387efa882881f8f8
parisa pahlevan <paris.pahlevan@gmail.com>
2018-06-27 02:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var obj = { foo: 'bar' }
- 
-var newObj = Object.assign({}, obj)
+var recipes = {egg: "2"};
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object)
+  newObj[key] = value
+  return newObj
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+       var newObj = Object.assign(delete[key])
+       newObj= (delete[key])
+       return newObj
+     }
+     
+     function destructivelyDeleteFromObjectByKey(object, key){
+       delete object[key]
+       return object
+     }
+    
